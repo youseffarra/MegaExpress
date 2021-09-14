1dee6da090ee31cfc6a323e53ac8b74fc7f10c86
youseffarra <77879852+youseffarra@users.noreply.github.com>
2021-09-14 13:43:21 +0300

    My first commit! 😀

---
+++ js/script.js
@@ -0,0 +1,69 @@
+$( document ).ready(function() {
+    $('#m-search').click(function() {
+        $('.search-form-wrapper').removeClass('d-none');
+        $('.search-form-wrapper').toggleClass('d-block');
+    });
+    $('#c-search').click(function() {
+        $('.search-form-wrapper').toggleClass('d-none');
+        $('.search-form-wrapper').removeClass('d-block');
+    });
+
+    $('.img-slider').owlCarousel({
+        rtl:true,
+        loop:true,
+        margin:10,
+        nav:true,
+        autoplay:true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause:true,
+        items: 1,
+        
+    });
+
+    $('.cards-slider').owlCarousel({
+        rtl:true,
+        loop:true,
+        nav:true,
+        items: 5,
+        dots: false,
+
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 2, 
+            },
+            // breakpoint from 480 up
+            480 : {
+                items: 2,
+            },
+            // breakpoint from 768 up
+            768 : {
+                items: 3,
+            },
+            1000: {
+                items: 5,
+            }
+        }
+        
+    });
+
+
+    $('.menuButO').click(function() {
+        $('.sidenav').css({"width": "70%", "height": "100%"});
+        $('.sidenav').removeClass('d-none');
+    });
+    $('.menuCloseBut').click(function() {
+        $('.sidenav').css({"width": "0", "height": "0"});
+        $('.sidenav').toggleClass('d-none');
+    });
+
+
+
+
+
+
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+    });
